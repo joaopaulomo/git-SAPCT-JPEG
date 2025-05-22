9d4947e6ab6f4df3154c0b55b1407dedcf18008d
Gustavo Coelho <coelho1914@gmail.com>
2025-05-22 10:40:33 -0300

    Add prints dos resultados

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ pot = 2
 result_soma = soma(a, b)
 result_sub = subtracao(a, b)
 result_pot = potencia(a, pot)
+
+print(f"resultado soma: {result_soma}")
+print(f"resultado subtração: {result_sub}")
+print(f"resultado potência: {result_pot}")
