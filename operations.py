0773df9edf8693e5cb7a15d2bedb32305862803c
joaopaulomo <92831575+joaopaulomo@users.noreply.github.com>
2025-05-23 15:07:27 -0300

    Update operations.py

--- operations.py
+++ operations.py
@@ -10,8 +10,8 @@ def potencia(a, pot):
 def multiplicacao(a, b):
     return a * b
 
-def divisao(a,b):
-    if b!=0:
-        return a/b
-    else:
+def divisao(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
         return "Erro: divisão por zero"
